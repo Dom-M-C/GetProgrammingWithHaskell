e267152f7a39b9648d74347d8caffdc6c418e880
Dmc <shen.nung@gmail.com>
2019-01-02 18:48:56 +0000

    moving onto chapter 14

--- src/Chapter13.hs
+++ src/Chapter13.hs
@@ -1,1 +1,13 @@
 module Chapter13 where
+
+import Data.List
+
+data Icecream = Vanilla | Chocolate deriving(Eq, Show, Ord, Enum, Bounded)
+
+sortedIcecream = sort [Chocolate, Vanilla, Chocolate, Vanilla]
+
+
+cycleSucc :: (Bounded a, Enum a, Eq a) => a -> a
+cycleSucc x
+    | x == maxBound = minBound
+    | otherwise = succ x
