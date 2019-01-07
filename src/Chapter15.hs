1b241b73fcd34589e64eae10eb53a8803bb51cd0
Dom-M-C <dominic.m.copeland@gmail.com>
2019-01-07 20:39:52 +0000

    completed encoder/decoder with one-time pad. Up to listing 15.5 - a cipher typeclass

--- src/Chapter15.hs
+++ src/Chapter15.hs
@@ -18,12 +18,12 @@ instance Bounded CipherAlphabet where
     minBound = LowerCaseChar 'a'
     maxBound = LowerCaseChar 'z'
 -}
-class LowerCaseChar a => Cipher a where
+class LowerCaseChar a => CipherZ a where
     encrypt :: a -> a
     decrypt :: a -> a
 
 
-instance Cipher Char where
+instance CipherZ Char where
     encrypt c
         | c == ' ' = ' '
         | c `elem` (map (toEnum) [48..57] :: [Char]) = c
@@ -61,12 +61,73 @@ type Bits = [Bool]
 intToBits' :: Int -> Bits
 intToBits' 0 = [False]
 intToBits' 1 = [True]
-intToBits' n = let
-    remainder = n `mod` 2
-    nextVal = n `div` 2
-    in if remainder == 0
+intToBits' n =
+    let
+        remainder = n `mod` 2
+        nextVal = n `div` 2
+    in
+        if remainder == 0
         then False : intToBits' nextVal
         else True : intToBits' nextVal
 
---15.6
+--15.16
+
+maxBits :: Int
+maxBits = length (intToBits' maxBound)
+
+intToBits :: Int -> Bits
+intToBits n =
+    let
+        reversedBits = reverse $ intToBits' n
+        missingBits = maxBits - (length reversedBits)
+        leadingFalses = take missingBits (cycle [False])
+    in
+        leadingFalses ++ reversedBits
+
+charToBits :: Char -> Bits
+charToBits c = intToBits $ fromEnum c
+
+--bitsToInt :: Bits -> Int
+bitsToInt bits@(b:bs) =
+    let
+        size = length bits
+        indices = [size-1, size-2 ..0]
+        trueLocations = filter (\x -> fst x == True) $ zip bits indices
+    in
+        sum $ map (\x -> 2 ^ snd x) trueLocations
+
+bitsToChar :: Bits -> Char
+bitsToChar bits = toEnum (bitsToInt bits)
+
+myPad :: String
+myPad = "salty"
+
+myPlainText :: String
+myPlainText = "hello cryptography"
+
+stringToBits :: String -> [Bits]
+stringToBits = map charToBits
+
+bitsToString :: [Bits] -> String
+bitsToString = map bitsToChar
+
+applyPad' :: String -> String -> [Bits]
+applyPad' pad text =
+    let
+        paddedPad = take (length text) $ cycle pad
+        padAsBits = stringToBits paddedPad
+        textAsBits = stringToBits text
+        zippedBits = zip padAsBits textAsBits
+    in
+        map (\pair -> fst pair `xor` snd pair ) zippedBits
+
+applyPad :: String -> String -> String
+applyPad pad text = bitsToString $ applyPad' pad text
+
+encodeDecodeWithMyPad :: String -> String
+encodeDecodeWithMyPad = applyPad myPad
+
+
+
+
 
