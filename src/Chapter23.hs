55ce05ea63ffe30680a4ac848ab6f4a6f86d5456
Dom-M-C <dominic.m.copeland@gmail.com>
2019-01-27 22:33:20 +0000

    up to QC 23.3

---
+++ src/Chapter23.hs
@@ -0,0 +1,16 @@
+module Chapter23 where
+
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.Text as T
+
+aWord :: T.Text
+aWord = "Cheese"
+
+
+sampleInput :: T.Text
+sampleInput = "this\nis\ninput"
+
+someText :: T.Text
+someText = "Some\ntext for\t you"
+
+--do QC 23.3
