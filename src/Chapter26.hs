93c5dcdcadffb247d0dd902f68e06409189e8b3d
Dom-M-C <dominic.m.copeland@gmail.com>
2019-02-04 19:43:10 +0000

    up to 26.2.2

--- src/Chapter26.hs
+++ src/Chapter26.hs
@@ -1,1 +1,63 @@
 module Chapter26 where
+
+import qualified Data.ByteString as B
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Encoding as E
+
+import Data.Maybe
+
+type Author = T.Text
+type Title = T.Text
+
+data Book = Book
+    {   author :: Author
+    ,   title :: Title } deriving Show
+
+type Html = T.Text
+
+booksToHtml :: [Book] -> Html
+booksToHtml books = mconcat
+    [   "<html>\n"
+    ,   "<head><title>books</title>"
+    ,   "<meta charset='utf-8'>"
+    ,   "<head>\n"
+    ,   "<body>\n"
+    ,   booksHtml
+    ,   "\n</body>\n"
+    ,   "</html>"
+    ]
+    where
+        booksHtml = mconcat $ map bookToHtml books
+
+bookToHtml :: Book -> Html
+bookToHtml book = mconcat
+    [   "<p>\n"
+    ,   titleInTags
+    ,   authorInTags
+    ,   "</p>\n"
+    ]
+    where
+        titleInTags = mconcat ["<strong>", title book, "</strong>\n"]
+        authorInTags = mconcat ["<em>", author book, "</em>\n"]
+
+book1 :: Book
+book1 = Book
+    {   title = "The Conspiracy Against the Human Race"
+    ,   author = "Ligotti, Thomas" }
+
+book2 :: Book
+book2 = Book
+    {   title = "A Short History of Decay"
+    ,   author = "Cioran, Emil" }
+
+book3 :: Book
+book3 = Book
+    {   title = "The Tastes of Eros"
+    ,   author = "Bataille, Georges" }
+
+myBooks :: [Book]
+myBooks =  [book1,book2,book3]
+
+mainBooks :: IO ()
+mainBooks = TIO.writeFile "books.html" $ booksToHtml myBooks
