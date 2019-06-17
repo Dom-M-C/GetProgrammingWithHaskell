7bf2f217fba637a50253aa035b0916ca037dac48
Dom-M-C <dominic.m.copeland@gmail.com>
2019-06-17 17:36:57 +0100

    testing primes

--- Lesson37-primes/src/Primes.hs
+++ Lesson37-primes/src/Primes.hs
@@ -1,3 +1,18 @@
 module Primes where
 
+upperBound = 25000
 
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (this:rest) = this : sieve noFactors
+    where
+        noFactors = filter (not . (==0) . (`mod` this)) rest
+
+primes :: [Int]
+primes = sieve [2 .. upperBound]
+
+isPrime :: Int -> Maybe Bool
+isPrime n
+    | n < 2 = Nothing
+    | n > length primes = Nothing
+    | otherwise = Just (n `elem` primes)
