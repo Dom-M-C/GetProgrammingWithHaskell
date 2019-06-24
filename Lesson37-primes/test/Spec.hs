1cd60827ba78c23e10284a9b8f81fe77fa76fdb8
Dom-M-C <dominic.m.copeland@gmail.com>
2019-06-24 16:49:12 +0100

    primes quickcheck finished

--- Lesson37-primes/test/Spec.hs
+++ Lesson37-primes/test/Spec.hs
@@ -27,8 +27,26 @@ prop_nonPrimesAreComposite val =
 
 divisors val = filter ((==0) . (val `mod`)) [2 .. (val -  1)]
 
+prop_factorsMakeOriginal val =
+    if result == Nothing
+    then True
+    else product (fromJust result) == val
+    where
+        result = primeFactors val
+
+prop_allFactorsPrime val =
+    if result == Nothing
+    then True
+    else all (\x -> isPrime x == Just True) $ fromJust result
+    where
+        result = primeFactors val
+
+
+
 main :: IO ()
 main = do
     quickCheckWith qcArgs prop_validPrimesOnly
     quickCheckWith qcArgs prop_primesArePrime
     quickCheckWith qcArgs prop_nonPrimesAreComposite
+    quickCheckWith qcArgs prop_factorsMakeOriginal
+    quickCheckWith qcArgs prop_allFactorsPrime
