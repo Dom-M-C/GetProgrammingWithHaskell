eabab6f7369940a4dc9f59e15932f9e7d5047ec1
Dom-M-C <dominic.m.copeland@gmail.com>
2019-07-01 18:49:57 +0100

    starting chapter 38

--- lesson38/src/Lib.hs
+++ lesson38/src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib where
+
+import qualified Data.Text as T
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+myTake :: Int -> [a] -> [a]
+myTake 0 _ = []
+myTake n xs = head xs : myTake (n-1) (tail xs)
+
+myTakePm :: Int -> [a] -> [a]
+myTakePm 0 _ = []
+myTakePm n (x:xs) = x: myTakePm (n-1) xs
+
+textHead :: T.Text -> T.Text
+textHead x = case T.uncons x of
+    Just (y, _) -> T.pack [y]
+    Nothing -> ""
