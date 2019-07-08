62b9566cfe0cba878e8eceebfe17d93e95060b68
Dom-M-C <dominic.m.copeland@gmail.com>
2019-07-08 18:44:28 +0100

    up to 39.3

--- http-lesson/app/Main.hs
+++ http-lesson/app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as LC
+
+import Network.HTTP.Simple
+
 import Lib
 
 main :: IO ()
 main = someFunc
+
+
+
+baseUrl endpoint = B.concat ["https://", endpoint]
+
+token :: BC.ByteString
+token = "zCtSqkkpdOnIWKEcUjiGdrOXvTZAcQuP"
+
+noaaHost :: BC.ByteString
+noaaHost = "www.ncdc.noaa.gov"
+
+apiPath :: BC.ByteString
+apiPath = "/cdo-web/api/v2/datasets"
