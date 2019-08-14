8a9a601c0ce1580feeae4edb18e97889d647b504
Dom-M-C <dominic.m.copeland@gmail.com>
2019-08-14 17:40:39 +0100

    created sqlite database for chapter 41

--- db-lesson/build_db.sql
+++ db-lesson/build_db.sql
@@ -3,7 +3,7 @@ Drop table if exists Checkedout;
 Drop table if exists Tool;
 Drop table if exists Users;
 
-Create table Users
+Create table [User]
 (   Id Integer Primary Key
 ,   UserName Text
 );
@@ -21,11 +21,11 @@ Create table Checkedout
 ,   ToolId Integer
 );
 
-insert into Users (UserName) values('willkurt')
-insert into Users (UserName) values('Dom')
+insert into [User] (UserName) values('willkurt');
+insert into [User] (UserName) values('Dom');
 
 insert into Tool (Name, Description, LastReturned, TimesBorrowed)
-values('hammer', 'hits stuff', '2017-01-01', 0)
+values('hammer', 'hits stuff', '2017-01-01', 0);
 
 insert into Tool (Name, Description, LastReturned, TimesBorrowed)
-values('saw', 'cuts stuff', '2017-01-01', 0)
+values('saw', 'cuts stuff', '2017-01-01', 0);
