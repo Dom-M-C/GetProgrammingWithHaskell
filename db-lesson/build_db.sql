4fa8b111cf5e85f951d9c58bb5a2c874107b9d5b
Dom-M-C <dominic.m.copeland@gmail.com>
2020-05-09 17:31:07 +0100

    finished chapter 41

--- db-lesson/build_db.sql
+++ db-lesson/build_db.sql
@@ -3,31 +3,40 @@ Drop table if exists Checkedout;
 Drop table if exists Tool;
 Drop table if exists Users;
 
-Create table [User]
+Create Table [User]
 (   Id Integer Primary Key
-,   UserName Text
+,   UserName Text Unique
 
-,   CONSTRAINT UK_User_UserName UNIQUE (UserName)
+,   Constraint UK_User_UserName Unique (UserName)
 );
 
-Create table Tool
+Create Table Tool
 (   Id Integer Primary Key
-,   Name Text UNIQUE
+,   Name Text Unique
 ,   Description Text
 ,   LastReturned Text
 ,   TimesBorrowed Integer
+,   QuantityOwned Integer
 );
 
-Create table Checkedout
+Create Table Checkedout
 (   UserId Integer
 ,   ToolId Integer
+,   QuantityCheckedout Integer Not Null Default 0
+        Check(QuantityCheckedout >= 0)
+
+,   Constraint PK_Checkedout Primary Key(UserId, ToolId)
+,   Constraint FK_Checkedout_User Foreign Key(UserId)
+        REFERENCES [User](Id)
+,   Constraint FK_Checkedout_Tool Foreign Key(ToolId)
+        REFERENCES Tool(Id)
 );
 
 insert into [User] (UserName) values('willkurt');
 insert into [User] (UserName) values('Dom');
 
-insert into Tool (Name, Description, LastReturned, TimesBorrowed)
-values('hammer', 'hits stuff', '2017-01-01', 0);
+insert into Tool (Name, Description, LastReturned, TimesBorrowed, QuantityOwned)
+values('hammer', 'hits stuff', '2017-01-01', 0, 1);
 
-insert into Tool (Name, Description, LastReturned, TimesBorrowed)
-values('saw', 'cuts stuff', '2017-01-01', 0);
+insert into Tool (Name, Description, LastReturned, TimesBorrowed, QuantityOwned)
+values('saw', 'cuts stuff', '2017-01-01', 0, 1);
